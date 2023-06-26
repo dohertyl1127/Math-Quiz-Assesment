b2b1b0474a016b8c58853fdb7744247cc8250431
Dohertyl1127 <111251142+dohertyl1127@users.noreply.github.com>
2023-06-26 14:55:41 +1200

    Add files via upload

--- 05_Math_Quiz_Assessment/00_MQA_Sandbox.py
+++ 05_Math_Quiz_Assessment/00_MQA_Sandbox.py
@@ -1,5 +1,6 @@
 import random
 
+
 def difficulty_checker():
     diff_level = ""
     while diff_level == "":
@@ -7,21 +8,21 @@ def difficulty_checker():
         level = input("Please enter the difficulty level (easy, medium, hard): ")
 
         if level.lower() == "easy" or level.lower() == "e":
-                diff_level = "easy"
-                break
-                # Perform actions for Easy level here
+            diff_level = "easy"
+            break
+            # Perform actions for Easy level here
         elif level.lower() == "medium" or level.lower() == "m":
-                diff_level = "medium"
-                break
-                # Perform actions for Medium level here
+            diff_level = "medium"
+            break
+            # Perform actions for Medium level here
         elif level.lower() == "hard" or level.lower() == "h":
-                diff_level = "hard"
-                break
-                # Perform actions for Hard level here
+            diff_level = "hard"
+            break
+            # Perform actions for Hard level here
         else:
-                print("Invalid difficulty level entered. Please try again.")
-                # Optionally, you can choose to recursively call the function again to prompt for a valid difficulty level
-                # difficulty_checker()
+            print("Invalid difficulty level entered. Please try again.")
+            # Optionally, you can choose to recursively call the function again to prompt for a valid difficulty
+            # level difficulty_checker()
     return diff_level
 
 
@@ -49,10 +50,69 @@ def check_questions():
 
 
 def generate_random_equation():
-    operators = ['+', '-', '*', '/']
-    operator = random.choice(operators)
+    # operators = ['+', '-', '*', '/']
+    # operator = random.choice(operators)
     if diff_level == "easy":
+        operators = ['+', '-']
+        operator = random.choice(operators)
 
+        # Generate two random operands
+        operand1 = random.randint(1, 10)
+        operand2 = random.randint(1, 10)
+        equation = f"{operand1} {operator} {operand2}"
+        if operator == '+':
+            answer = operand1 + operand2
+        elif operator == '-':
+            answer = operand1 - operand2
+    elif diff_level == "medium":
+        operators = ['+', '-', '*', '/']
+        operator = random.choice(operators)
+
+        # Generate two random operands
+        operand1 = random.randint(1, 10)
+        operand2 = random.randint(1, 10)
+        equation = f"{operand1} {operator} {operand2}"
+
+        if operator == '/':
+            # Ensure the division is exact by generating a random numerator and denominator
+            denominator = random.randint(1, 10)
+            numerator = random.randint(1, 10) * denominator
+            equation = f"{numerator} {operator} {denominator}"
+            answer = numerator // denominator
+        if operator == '+':
+            answer = operand1 + operand2
+        elif operator == '-':
+            answer = operand1 - operand2
+        elif operator == '*':
+            answer = operand1 * operand2
+    elif diff_level == "hard":
+        operators = ['+', '-', '*', '/']
+        operator = random.choice(operators)
+
+        # Generate two random operands
+        operand1 = random.randint(1, 100)
+        operand2 = random.randint(1, 100)
+        equation = f"{operand1} {operator} {operand2}"
+
+        if operator == '/':
+            # Ensure the division is exact by generating a random numerator and denominator
+            denominator = random.randint(1, 100)
+            numerator = random.randint(1, 100) * denominator
+            equation = f"{numerator} {operator} {denominator}"
+            answer = numerator // denominator
+        if operator == '+':
+            answer = operand1 + operand2
+        elif operator == '-':
+            answer = operand1 - operand2
+        elif operator == '*':
+            answer = operand1 * operand2
+
+        if operator == '+':
+            answer = operand1 + operand2
+        elif operator == '-':
+            answer = operand1 - operand2
+        elif operator == '*':
+            answer = operand1 * operand2
     if operator == '/':
         # Ensure the division is exact by generating a random numerator and denominator
         denominator = random.randint(1, 10)
@@ -76,7 +136,16 @@ def generate_random_equation():
     return equation, answer
 
 
-equation, answer = generate_random_equation()
-print("Equation:", equation)
-print("Answer:", answer)
+# main routine
+diff_level = difficulty_checker()
 
+print(f"you select {diff_level} difficulty")
+print()
+
+questions = check_questions()
+print(questions, "questions")
+
+for item in range(1, questions):
+    equation, answer = generate_random_equation()
+    print("Equation:", equation)
+    print("Answer:", answer)
