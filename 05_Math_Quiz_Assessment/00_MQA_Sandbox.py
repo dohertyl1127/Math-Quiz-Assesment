8d84bf1dbfc265b813fdb0d8578a159e98a61bd5
Dohertyl1127 <111251142+dohertyl1127@users.noreply.github.com>
2023-07-16 19:43:55 +1200

    Add files via upload

--- 05_Math_Quiz_Assessment/00_MQA_Sandbox.py
+++ 05_Math_Quiz_Assessment/00_MQA_Sandbox.py
@@ -1,7 +1,9 @@
+# imports
 import random
+import math
 
 
-# functions
+# Function to check if user input is an integer
 def int_checker(question):
     while True:
         user_input = input(question)
@@ -12,101 +14,97 @@ def int_checker(question):
             print("Invalid input. Please enter a number.")
 
 
+# Function to generate a statement with decoration
 def statement_gen(statement, decoration):
     sides = decoration * 3
-
     statement = "{} {} {}".format(sides, statement, sides)
-
     top_bottom = decoration * len(statement)
-
     print(top_bottom)
     print(statement)
     print(top_bottom)
-
     return ""
 
 
+# Function to display instructions
+def instructions():
+    print("This is a basic math quiz. In this quiz, you will choose from ")
+    print("easy, medium, and hard difficulties. Then, choose how many")
+    print("questions you will be asked. Try to answer the questions to")
+    print("the best of your ability and have fun!")
+    print()
+
+
+# Function to prompt for a yes or no response
+def yes_no(question):
+    while True:
+        response = input(question).lower()
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n":
+            return "no"
+        else:
+            print("Please choose yes/no")
+
+
+# Function to validate and return the difficulty level chosen
 def difficulty_checker():
     diff_level = ""
     while diff_level == "":
-
         level = input("Please enter the difficulty level (easy, medium, hard): ")
-
         if level.lower() == "easy" or level.lower() == "e":
             diff_level = "easy"
             break
-            # Perform actions for Easy level here
         elif level.lower() == "medium" or level.lower() == "m":
             diff_level = "medium"
             break
-            # Perform actions for Medium level here
         elif level.lower() == "hard" or level.lower() == "h":
             diff_level = "hard"
             break
-            # Perform actions for Hard level here
         else:
             print("Invalid difficulty level entered. Please try again.")
-            # Optionally, you can choose to recursively call the function again to prompt for a valid difficulty level
-            # difficulty_checker()
     return diff_level
 
 
+# Function to check the number of questions input by the user
 def check_questions():
     while True:
-        response = input("how many questions? ")
-
-        round_error = "please type either <enter>" \
-                      "or an integer that is more than 0"
-
+        response = input("How many questions? ")
+        round_error = "Please type either <enter> or an integer that is more than 0"
         if response != "":
             try:
-
                 response = int(response)
-
                 if response < 1:
                     print(round_error)
                     continue
-
             except ValueError:
                 print(round_error)
                 continue
-
         return response
 
 
+# Function to generate a random equation based on difficulty level
 def generate_random_equation():
     if diff_level == "easy":
         operators = ['+', '-']
         operator = random.choice(operators)
-
-        # Generate two random operands
         operand1 = random.randint(1, 10)
         operand2 = random.randint(1, 10)
     elif diff_level == "medium":
         operators = ['+', '-', '*', '/']
         operator = random.choice(operators)
-
-        # Generate two random operands
         operand1 = random.randint(1, 10)
         operand2 = random.randint(1, 10)
-
         if operator == '/':
-            # Ensure the division is exact by generating a random numerator and denominator
             denominator = random.randint(1, 10)
             numerator = random.randint(1, 10) * denominator
     elif diff_level == "hard":
         operators = ['+', '-', '*', '/']
         operator = random.choice(operators)
-
-        # Generate two random operands
         operand1 = random.randint(1, 100)
         operand2 = random.randint(1, 100)
-
         if operator == '/':
-            # Ensure the division is exact by generating a random numerator and denominator
             denominator = random.randint(1, 100)
             numerator = random.randint(1, 100) * denominator
-    # define equation and answer
     equation = f"{operand1} {operator} {operand2}"
     if operator == '+':
         answer = operand1 + operand2
@@ -117,10 +115,10 @@ def generate_random_equation():
     elif operator == '/':
         equation = f"{numerator} {operator} {denominator}"
         answer = numerator // denominator
-
     return equation, answer
 
 
+# Function to check if the user's answer is correct
 def check_answer(question, correct_answer, difficulty):
     if difficulty == 'easy':
         attempts = 4
@@ -129,11 +127,9 @@ def check_answer(question, correct_answer, difficulty):
     attempts_taken = 0
     attempts_left = attempts
     for item in range(attempts):
-        wrong = "Sorry that is incorrect"
-        right = "That's correct!"
-        wrong_last = "sorry you've run out of guesses"
+        wrong = "Sorry, that is incorrect"
         attempts_left -= 1
-        print(f"you have {attempts_left + 1} attempts ")
+        print(f"You have {attempts_left + 1} attempts")
         user_answer = int_checker(question)
         if user_answer == correct_answer:
             return 'correct'
@@ -144,28 +140,54 @@ def check_answer(question, correct_answer, difficulty):
             attempts_taken += 1
 
 
-# main routine
-while True:
-    questions_attempted = 0
-    questions_wrong = 0
-    questions_right = questions_attempted - questions_wrong
+# Function to display the game summary
+def game_summary(attempted, right, wrong):
+    percent_right = right / attempted * 100
+    percent_wrong = wrong / attempted * 100
+    print(f"Wins: {right} ({round(percent_right, 2)}%)")
+    print(f"Loss: {wrong} ({round(percent_wrong, 2)}%)")
 
-    diff_level = difficulty_checker()
 
-    print(f"you select {diff_level} difficulty")
+# Main routine
+questions_attempted = 0
+questions_wrong = 0
+questions_right = questions_attempted - questions_wrong
+
+# Prints the game title
+statement_gen("Welcome to my math quiz!", "=")
+
+# Asks the user if they have played before and provides instructions if not
+print()
+instruct = yes_no("Have you played before?")
+print()
+
+if instruct == "no":
+    statement_gen("Instructions: ", "*")
+    print()
+    instructions()
+
+# Asks the user for the level of difficulty
+diff_level = difficulty_checker()
+
+# Asks the user for the number of questions
+questions = check_questions()
+print(f"You will be asked {questions} questions")
+print()
+
+# Loop to generate and check each question
+for item in range(questions):
+    equation, answer = generate_random_equation()
+    statement_gen(f"Question {questions_attempted + 1} of {questions}", "=")
+    wrong_right = check_answer(equation, answer, diff_level)
+    if wrong_right == 'correct':
+        statement_gen("Correct!", "+")
+        questions_right += 1
+    elif wrong_right == 'wrong':
+        statement_gen("Sorry, you got it wrong", "-")
+        questions_wrong += 1
+    questions_attempted += 1
     print()
 
-    questions = check_questions()
-    print(questions, "questions")
-
-    for item in range(questions):
-        equation, answer = generate_random_equation()
-        statement_gen(f"question {questions_attempted + 1} of {questions}", "=")
-        wrong_right = check_answer(equation, answer, diff_level)
-        if wrong_right == 'correct':
-            statement_gen("correct!", "+")
-        elif wrong_right == 'wrong':
-            statement_gen("sorry, you got it wrong", "-")
-            questions_wrong += 1
-        questions_attempted += 1
-        print()
+# Display game summary
+statement_gen("Game Summary: ", "#")
+game_summary(questions_attempted, questions_right, questions_wrong)
--- 05_Math_Quiz_Assessment/01_MQA_Base.py
+++ 05_Math_Quiz_Assessment/01_MQA_Base.py
@@ -3,7 +3,7 @@ import random
 import math
 
 
-# functions
+# Function to check if user input is an integer
 def int_checker(question):
     while True:
         user_input = input(question)
@@ -14,122 +14,97 @@ def int_checker(question):
             print("Invalid input. Please enter a number.")
 
 
-def statement_gen(statement, decoration, ):
+# Function to generate a statement with decoration
+def statement_gen(statement, decoration):
     sides = decoration * 3
-
     statement = "{} {} {}".format(sides, statement, sides)
-
     top_bottom = decoration * len(statement)
-
     print(top_bottom)
     print(statement)
     print(top_bottom)
-
     return ""
 
 
+# Function to display instructions
 def instructions():
-    print("Instructions go here")
+    print("This is a basic math quiz. In this quiz, you will choose from ")
+    print("easy, medium, and hard difficulties. Then, choose how many")
+    print("questions you will be asked. Try to answer the questions to")
+    print("the best of your ability and have fun!")
     print()
 
 
+# Function to prompt for a yes or no response
 def yes_no(question):
     while True:
-
-        # ask user if they have played before
         response = input(question).lower()
-
-        # if yes continue code
         if response == "yes" or response == "y":
             return "yes"
-        # if no display instructions
         elif response == "no" or response == "n":
             return "no"
         else:
-            print("please choose yes/no")
+            print("Please choose yes/no")
 
 
+# Function to validate and return the difficulty level chosen
 def difficulty_checker():
     diff_level = ""
     while diff_level == "":
-
         level = input("Please enter the difficulty level (easy, medium, hard): ")
-
         if level.lower() == "easy" or level.lower() == "e":
             diff_level = "easy"
             break
-            # Perform actions for Easy level here
         elif level.lower() == "medium" or level.lower() == "m":
             diff_level = "medium"
             break
-            # Perform actions for Medium level here
         elif level.lower() == "hard" or level.lower() == "h":
             diff_level = "hard"
             break
-            # Perform actions for Hard level here
         else:
             print("Invalid difficulty level entered. Please try again.")
-            # Optionally, you can choose to recursively call the function again to prompt for a valid difficulty level
-            # difficulty_checker()
     return diff_level
 
 
+# Function to check the number of questions input by the user
 def check_questions():
     while True:
-        response = input("how many questions? ")
-
-        round_error = "please type either <enter>" \
-                      "or an integer that is more than 0"
-
+        response = input("How many questions? ")
+        round_error = "Please type either <enter> or an integer that is more than 0"
         if response != "":
             try:
-
                 response = int(response)
-
                 if response < 1:
                     print(round_error)
                     continue
-
             except ValueError:
                 print(round_error)
                 continue
-
         return response
 
 
+# Function to generate a random equation based on difficulty level
 def generate_random_equation():
     if diff_level == "easy":
         operators = ['+', '-']
         operator = random.choice(operators)
-
-        # Generate two random operands
         operand1 = random.randint(1, 10)
         operand2 = random.randint(1, 10)
     elif diff_level == "medium":
         operators = ['+', '-', '*', '/']
         operator = random.choice(operators)
-
-        # Generate two random operands
         operand1 = random.randint(1, 10)
         operand2 = random.randint(1, 10)
-
         if operator == '/':
-            # Ensure the division is exact by generating a random numerator and denominator
             denominator = random.randint(1, 10)
             numerator = random.randint(1, 10) * denominator
     elif diff_level == "hard":
         operators = ['+', '-', '*', '/']
         operator = random.choice(operators)
-
-        # Generate two random operands
         operand1 = random.randint(1, 100)
         operand2 = random.randint(1, 100)
-
         if operator == '/':
-            # Ensure the division is exact by generating a random numerator and denominator
             denominator = random.randint(1, 100)
             numerator = random.randint(1, 100) * denominator
-    # define equation and answer
     equation = f"{operand1} {operator} {operand2}"
     if operator == '+':
         answer = operand1 + operand2
@@ -140,10 +115,10 @@ def generate_random_equation():
     elif operator == '/':
         equation = f"{numerator} {operator} {denominator}"
         answer = numerator // denominator
-
     return equation, answer
 
 
+# Function to check if the user's answer is correct
 def check_answer(question, correct_answer, difficulty):
     if difficulty == 'easy':
         attempts = 4
@@ -152,9 +127,9 @@ def check_answer(question, correct_answer, difficulty):
     attempts_taken = 0
     attempts_left = attempts
     for item in range(attempts):
-        wrong = "Sorry that is incorrect"
+        wrong = "Sorry, that is incorrect"
         attempts_left -= 1
-        print(f"you have {attempts_left + 1} attempts ")
+        print(f"You have {attempts_left + 1} attempts")
         user_answer = int_checker(question)
         if user_answer == correct_answer:
             return 'correct'
@@ -165,50 +140,54 @@ def check_answer(question, correct_answer, difficulty):
             attempts_taken += 1
 
 
+# Function to display the game summary
 def game_summary(attempted, right, wrong):
-
     percent_right = right / attempted * 100
     percent_wrong = wrong / attempted * 100
+    print(f"Wins: {right} ({round(percent_right, 2)}%)")
+    print(f"Loss: {wrong} ({round(percent_wrong, 2)}%)")
 
-    print(f"right: {right} ({round(percent_right, 2)}%) ")
-    print(f"wrong: {wrong} ({round(percent_wrong, 2)}%) ")
+
+# Main routine
+questions_attempted = 0
+questions_wrong = 0
+questions_right = questions_attempted - questions_wrong
+
+# Prints the game title
+statement_gen("Welcome to my math quiz!", "=")
+
+# Asks the user if they have played before and provides instructions if not
+print()
+instruct = yes_no("Have you played before?")
+print()
+
+if instruct == "no":
+    statement_gen("Instructions: ", "*")
+    print()
+    instructions()
+
+# Asks the user for the level of difficulty
+diff_level = difficulty_checker()
+
+# Asks the user for the number of questions
+questions = check_questions()
+print(f"You will be asked {questions} questions")
+print()
+
+# Loop to generate and check each question
+for item in range(questions):
+    equation, answer = generate_random_equation()
+    statement_gen(f"Question {questions_attempted + 1} of {questions}", "=")
+    wrong_right = check_answer(equation, answer, diff_level)
+    if wrong_right == 'correct':
+        statement_gen("Correct!", "+")
+        questions_right += 1
+    elif wrong_right == 'wrong':
+        statement_gen("Sorry, you got it wrong", "-")
+        questions_wrong += 1
+    questions_attempted += 1
     print()
 
-
-# Main routine goes here
-# set question amounts
-while end == "":
-    questions_attempted = 0
-    questions_wrong = 0
-    questions_right = questions_attempted - questions_wrong
-    # prints a game title
-    statement_gen("Welcome to my math quiz!", "=")
-    # ask user if they have played before and explains if not
-    print()
-    instruct = yes_no("Have you played before?")
-    print()
-
-    if instruct == "no":
-        instructions()
-    # asks user for level of difficulty
-    diff_level = difficulty_checker()
-    # asks user for amount of questions
-    questions = check_questions()
-    print(f"you will be asked {questions} questions")
-    print()
-
-    for item in range(questions):
-        equation, answer = generate_random_equation()
-        statement_gen(f"question {questions_attempted + 1} of {questions}", "=")
-        wrong_right = check_answer(equation, answer, diff_level)
-        if wrong_right == 'correct':
-            statement_gen("correct!", "+")
-            questions_right += 1
-        elif wrong_right == 'wrong':
-            statement_gen("sorry, you got it wrong", "-")
-            questions_wrong += 1
-        questions_attempted += 1
-        print()
-    statement_gen("Game Summary: ", "#")
-    game_summary(questions_attempted, questions_right, questions_wrong)
-    print("if you would like to play again press <enter> or xxx to exit")
+# Display game summary
+statement_gen("Game Summary: ", "#")
+game_summary(questions_attempted, questions_right, questions_wrong)
